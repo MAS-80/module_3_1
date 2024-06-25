58ed49e98c841df96fe6aa38ec5a55d0b0d503ae
MAS-80 <andrej.makruschin@yandex.ru>
2024-06-25 23:47:32 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,24 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info (string):
+    global calls
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+
+def is_contains (string, list_to_search):
+    global calls
+    count_calls()
+    string = string.lower()
+    for i in range(len(list_to_search)):
+       list_to_search[i] = list_to_search[i].lower()
+    return string in list_to_search
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban',['ban', 'BaNaN', 'urBan']))
+print(is_contains('cycle', ['recycle', 'cyclic']))
+print(calls)
